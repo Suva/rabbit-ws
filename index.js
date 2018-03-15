d4acd088067b9261a67f37bcd4bb63b02505bde2
Jaan Pullerits <clysuva@gmail.com>
2018-03-15 02:39:33 +0200

    Add slides

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const amqp = require('amqplib');
     const queue = await channel.assertQueue("messages", {durable: false, autoDelete: true});
 
     channel.consume(queue.name, (msg) => {
-        console.log(msg.content.toString())
+        console.log("Message received:", msg.content.toString())
         channel.ack(msg)
     })
 })();
@@ -14,3 +14,5 @@ async function getChannel() {
     const conn = await amqp.connect("amqp://rjsydvru:1s9kKNAhjd2mNWBUf3JIBSEA4R8ygAar@duckbill.rmq.cloudamqp.com/rjsydvru");
     return conn.createChannel();
 }
+
+// http://www.squaremobius.net/amqp.node/
